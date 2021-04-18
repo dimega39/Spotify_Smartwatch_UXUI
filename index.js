2a10e90f82759d90395a6c1bfa3c54a1c66ed3c1
Diana Mendoza <diana.mendoza093@gmail.com>
2021-04-18 14:33:37 -0500

    CSS/JS Changes

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
-/*
-Follow me on Twitter : @One_div
-or on Dribbble : dribbble.com/One_div
-*/
+$(document).ready(function(){
+    function clock (){
+        var d = new Date (),
+            h = d.getHours(),
+            m = d.getMinutes(),
+            s = d.getSeconds();
+        if(h < 10){ h = '0' + h; }
+        if(m < 10){ m = '0' + m; }
+        if(s < 10){ s = '0' + s; }
+        $('.hour').text(h);
+        $('.min').text(m);
+        $('.sec').text(s);
 
-$('#range').on("change", function() {
-    $('.wrapper').css('font-size',this.value +"px");
-    }).trigger("change");
+        setTimeout(clock,1000);
+    }
+    clock();
+});
